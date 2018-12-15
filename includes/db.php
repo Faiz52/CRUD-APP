271a4e0e078f196a79bd0dd07d1bf60532d0875b
Faizan ur Rehman <faizan.rehman52@gmail.com>
2018-12-15 14:29:41 +0500

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -7,6 +7,24 @@ define('DB_NAME','crudapp');
 
 $conn = mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
 
+
+function escape($string)
+{
+    global $conn;    
+    return mysqli_real_escape_string($conn,$string);
+}
+
+function clean($string)
+{
+	$string = trim($string);
+	$string = stripcslashes($string);
+	$string = htmlentities($string);
+
+	return $string;
+}
+
+
+
 ?>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
